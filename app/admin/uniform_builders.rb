3742f76d498601a614386468b81d103269b3b273
Ali Hassan <ali.hassan@virtual-force.com>
2017-12-12 19:51:14 +0500

    ## some bug fixed

--- app/admin/uniform_builders.rb
+++ app/admin/uniform_builders.rb
@@ -3,14 +3,16 @@ ActiveAdmin.register UniformBuilder do
 
 
 	form do |f|
-		f.input :style_id, as: :select,  collection: Style.all.collect{|style| [style.name, style.id]}
-		f.input :sport_id, as: :select,  collection: Sport.all.collect{|sport| [sport.name, sport.id]}
-		f.input :color_id, as: :select,  collection: Color.all.collect{|color| [color.name, color.id]}
-		f.input :gender_id, as: :select, collection: Gender.all.collect{|gender| [gender.name, gender.id]}
-		f.input :category
-		f.input :type
-		f.input :placement_id
-		f.input :photo
+		f.inputs "Details" do
+			f.input :style_id, as: :select,  collection: Style.all.collect{|style| [style.name, style.id]}
+			f.input :sport_id, as: :select,  collection: Sport.all.collect{|sport| [sport.category, sport.id]}
+			f.input :color_id, as: :select,  collection: Color.all.collect{|color| [color.color, color.id]}
+			f.input :gender_id, as: :select, collection: Gender.all.collect{|gender| [gender.name, gender.id]}
+			f.input :category
+			f.input :type
+			f.input :placement_id
+			f.input :photo
+		end	
 		f.actions
 	end
 
