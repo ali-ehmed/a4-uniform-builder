3620681fac687da2ac3e22cc5e7aff9bbe2cbc81
Ali Hassan <ali.hassan@virtual-force.com>
2017-12-24 11:54:52 +0500

    ## Fixes

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -3,10 +3,12 @@ class StylesController < ApplicationController
     @style = Style.create style_options_params
   end
   def index
-    @styles = Style.all
+    @style_category = params[:category]  
+    @styles         = Style.all
   end
   private
   def style_options_params
-    params.require(:style).permit(:description, :name, :category, :colour, :image, colors: [], sizes: [], placements: [], decorations:[])
-  end
+    params.require(:style).permit(:style_code,:acs_style,:sport_id,:gender_id,:style_category,:color_description,:category_id,
+    :style_features,:front,:back, :thumbnail,colors: [], sizes: [],placements: [],decorations:[])
+  end  
 end
