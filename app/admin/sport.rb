10683bd981998e4d245377a96eb26e551206223c
Ali Hassan <ali.hassan@virtual-force.com>
2017-12-25 22:44:23 +0500

    ## added sport name in adminpanel

--- app/admin/sport.rb
+++ app/admin/sport.rb
@@ -1,4 +1,4 @@
 ActiveAdmin.register Sport do
   menu parent: "Uniform Builder"
-  permit_params :acs_sales_category_code, :acs_sales_category_description,:marketing_category, :image, :sport_image
+  permit_params :sport_name, :acs_sales_category_code, :acs_sales_category_description,:marketing_category, :image, :sport_image
 end
