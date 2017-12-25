88138b734148b55d86ce769a68ad900c281cb595
Ali Hassan <ali.hassan@virtual-force.com>
2017-12-25 22:22:45 +0500

    ## added missing fields and changed more informstion link to select

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171223135929) do
+ActiveRecord::Schema.define(version: 20171225172123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -192,6 +192,7 @@ ActiveRecord::Schema.define(version: 20171223135929) do
     t.datetime "updated_at", null: false
     t.string "sport_image"
     t.boolean "is_selected"
+    t.string "sport_name"
   end
 
   create_table "style_colors", force: :cascade do |t|
