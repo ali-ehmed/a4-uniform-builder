60f18c63c03366b43e8678208d4ec518d75e7bfa
Ali Hassan Mirza <a.hassan@ssidecisions.com>
2017-12-28 13:46:21 +0500

    ## Options page integrated.

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -8,7 +8,7 @@ class OptionsController < ApplicationController
   end 
   private
   def update_user
-    params[:style]  && current_user.update_attribute(:style, params[:style])
-    params[:design] && current_user.update_attribute(:design, params[:design])
+    params[:style]  && current_user.update_attribute(:style, params[:style])    && (@style = Style.find_by_id(params[:style]))
+    params[:design] && current_user.update_attribute(:design, params[:design])  && (@style = Design.find_by_id(params[:design]))
   end
 end
