743b97cbbe2c14607d40ce6cfed18cb2c5d8c61a
Ali Hassan Mirza <Ali.hassan.mirzaa@gmail.com>
2018-01-01 22:16:55 +0500

    ## changes

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -4,13 +4,16 @@ class OptionsController < ApplicationController
 
   end
   def index
-    @tile_1 = @style.try(:colors).where(is_tile_one:    true)
-    @tile_2 = @style.try(:colors).where(is_tile_two:    true)
-    @tile_3 = @style.try(:colors).where(is_tile_three:  true)
+    @style && tiles
   end
   private
   def update_user
     params[:style]  && current_user.update_attribute(:style, params[:style])    && (@style = Style.find_by_id(params[:style]))
     params[:design] && current_user.update_attribute(:design, params[:design])  && (@style = Design.find_by_id(params[:design]))
+  end
+  def tiles
+    @tile_1 = @style.try(:colors).where(is_tile_one:    true)
+    @tile_2 = @style.try(:colors).where(is_tile_two:    true)
+    @tile_3 = @style.try(:colors).where(is_tile_three:  true)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
       raw file.read
     end
   end
+  def sport
+    current_user && Sport.find_by_id(current_user.sport)
+  end
+  def gender
+    Gender.find_by_id(sport.try(:gender_id))
+  end
+  def category
+    Category.find_by_id(current_user.category_id)
+  end
 end
