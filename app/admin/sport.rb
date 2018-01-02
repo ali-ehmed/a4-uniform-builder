67c0f21c3152ce00a97681791e01904a2d561d03
Ali Hassan Mirza <a.hassan@ssidecisions.com>
2018-01-02 17:32:48 +0500

    ## hide sports fields in CMS

--- app/admin/sport.rb
+++ app/admin/sport.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Sport do
   menu parent: "Uniform Builder"
-  permit_params :is_featured, :sport_name, :acs_sales_category_code, :acs_sales_category_description,:marketing_category, :image, :sport_image
+  permit_params :is_featured, :sport_name, :image, :sport_image#, :acs_sales_category_code, :acs_sales_category_description,:marketing_category
   action_item :add, only: :show do
     link_to "New Sport", :new_admin_sport
   end
